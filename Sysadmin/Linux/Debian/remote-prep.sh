a214426afdaf90533ce443de146390be7fbc886b
Steve Clement <steve@localhost.lu>
2021-12-08 16:03:19 +0100

    chg: [upd] more tweaks

--- Sysadmin/Linux/Debian/remote-prep.sh
+++ Sysadmin/Linux/Debian/remote-prep.sh
@@ -12,6 +12,11 @@ fi
 alias R_SSH="ssh -q -t $REMOTE"
 
 REMOTE_USER=$(R_SSH whoami)
+REMOTE_HOME=$(R_SSH pwd)
+if [[ -z ${REMOTE_USER} ]]; then
+    echo "Cannot determine user failed, abort"
+    exit 255
+fi
 echo "..........................................."
 echo "Installing for remote user ${REMOTE_USER}"
 echo "..........................................."
@@ -20,36 +25,39 @@ sleep 3
 R_SSH sudo apt install etckeeper -y
 R_SSH sudo apt update
 R_SSH sudo apt dist-upgrade
+R_SSH sudo apt autoremove
 R_SSH sudo apt install command-not-found zsh zsh-syntax-highlighting tmux mlocate trash-cli tmuxinator htop ncdu bat gawk npm fzf coreutils net-tools neovim flake8 -y
 
 # .ssh config?
 # .gnupg forwarding for signing commits
 
-R_SSH mkdir -p ~/.config/nvim
-R_SSH mkdir -p ~/.config/bat/themes
-R_SSH mkdir -p ~/.tmux
-R_SSH mkdir -p ~/.dir_colors
-R_SSH mkdir ~/bin
-R_SSH wget -O ~/.config/bat/themes/OneHalfDark.tmTheme https://raw.githubusercontent.com/sonph/onehalf/master/sublimetext/OneHalfDark.tmTheme
+R_SSH mkdir -p config/nvim
+R_SSH mkdir -p config/bat/themes
+R_SSH mkdir -p tmux
+R_SSH mkdir -p .dir_colors
+R_SSH mkdir bin
+R_SSH wget -O config/bat/themes/OneHalfDark.tmTheme https://raw.githubusercontent.com/sonph/onehalf/master/sublimetext/OneHalfDark.tmTheme
 R_SSH batcat cache -b
 
 R_SSH sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-R_SSH rm ~/.zshrc
-R_SSH rm ~/.tmux.conf
-scp -q ~/bin/prettyping ${REMOTE}:~/bin//
-scp -q ~/bin/diff-so-fancy ${REMOTE}:~/bin
+RSH "ZSH=${REMOTE_HOME}/.oh-my-zsh zsh -f ${REMOTE_HOME}/.oh-my-zsh/tools/upgrade.sh --interactive"
+R_SSH rm .zshrc
+R_SSH rm .tmux.conf
+scp -q ~/bin/prettyping ${REMOTE}:bin/
+scp -q ~/bin/diff-so-fancy ${REMOTE}:bin/
 scp -q ~/dotfiles/termite.terminfo ${REMOTE}:/tmp/
 R_SSH tic -x /tmp/termite.terminfo
 R_SSH sudo tic -x /tmp/termite.terminfo
 scp -q ~/dotfiles/.zshrc-remote ${REMOTE}:.zshrc
 scp -q ~/dotfiles/.dir_colors/dircolors ${REMOTE}:.dir_colors/
 scp -q ~/dotfiles/.gitconfig-remote ${REMOTE}:.gitconfig
+scp -q ~/dotfiles/.lessfilter ${REMOTE}:.lessfilter
 scp -q ~/dotfiles/.gitignore_global ${REMOTE}:.gitignore_global
-scp -q ~/dotfiles/.config/nvim/init.vim ${REMOTE}:~/.config/nvim/
-scp -q ~/.tmux/tmux.conf ${REMOTE}:~/.tmux/tmux.conf
-scp -q ~/.tmux/tmux.remote.conf ${REMOTE}:~/.tmux/tmux.remote.conf
-R_SSH ln -s ~/.tmux/tmux.conf ~/.tmux.conf
-R_SSH git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+scp -q ~/dotfiles/.config/nvim/init.vim ${REMOTE}:config/nvim/
+scp -q ~/.tmux/tmux.conf ${REMOTE}:tmux/tmux.conf
+scp -q ~/.tmux/tmux.remote.conf ${REMOTE}:tmux/tmux.remote.conf
+R_SSH ln -s tmux/tmux.conf tmux.conf
+R_SSH git clone https://github.com/tmux-plugins/tpm tmux/plugins/tpm
 # prefix + I -> Install plugs
-R_SSH curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+R_SSH curl -fLo .local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 R_SSH nvim +'PlugInstall' +qa --headless
